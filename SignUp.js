c89a44354ebfbea0f8bc74f9518c5efdc8d1fbb1
leenaO <46565265+leenaO@users.noreply.github.com>
2023-06-04 19:58:38 +0300

    Create SignUp.js

---
+++ SignUp.js
@@ -0,0 +1,7 @@
+$('button').click(function(){
+    let fname=$("#fname").val()
+    let lname=$("#lname").val()
+    let full_name=fname+" "+lname
+    alert("!Congratulations "+ full_name  +" you are now signed up")
+
+})
